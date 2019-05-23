05e30924a479986ea64329c906fa58f1f5c1abf7
jani-nykanen <jnyknn@gmail.com>
2019-05-23 22:37:22 +0300

    Test map and editor tiles added

--- dev/convert.sh
+++ dev/convert.sh
@@ -8,6 +8,9 @@ fi
 if [ ! -d "../ASSETS/BITMAPS" ]; then
     mkdir ../ASSETS/BITMAPS
 fi
+if [ ! -d "../ASSETS/MAPS" ]; then
+    mkdir ../ASSETS/MAPS
+fi
 
 # Bitmaps
 ../tools/png2bin bitmaps/font.png ../ASSETS/BITMAPS/FONT.BIN
@@ -15,3 +18,6 @@ fi
 ../tools/png2bin bitmaps/items.png ../ASSETS/BITMAPS/ITEMS.BIN
 ../tools/png2bin bitmaps/anim.png ../ASSETS/BITMAPS/ANIM.BIN
 ../tools/png2bin bitmaps/player.png ../ASSETS/BITMAPS/PLAYER.BIN
+
+# Maps
+../tools/tmx2bin maps/0.tmx ../ASSETS/MAPS/0.BIN
---
+++ dev/editor_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="editor_tiles" tilewidth="16" tileheight="16" tilecount="48" columns="16">
+ <image source="bitmaps/editor_tiles.png" width="256" height="48"/>
+</tileset>
