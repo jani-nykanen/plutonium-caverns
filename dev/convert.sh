a92d4d761acf47d1b9d1bc86b0f07b4446ba3320
jani-nykanen <jnyknn@gmail.com>
2019-05-23 22:15:22 +0300

    Convert script changed

--- dev/convert.sh
+++ dev/convert.sh
@@ -2,8 +2,16 @@
 cd "$(dirname "$0")"
 
 # Make folders
-mkdir ../ASSETS
-mkdir ../ASSETS/BITMAPS
+if [ ! -d "../ASSETS" ]; then
+    mkdir ../ASSETS
+fi
+if [ ! -d "../ASSETS/BITMAPS" ]; then
+    mkdir ../ASSETS/BITMAPS
+fi
 
 # Bitmaps
 ../tools/png2bin bitmaps/font.png ../ASSETS/BITMAPS/FONT.BIN
+../tools/png2bin bitmaps/tileset.png ../ASSETS/BITMAPS/TILESET.BIN
+../tools/png2bin bitmaps/items.png ../ASSETS/BITMAPS/ITEMS.BIN
+../tools/png2bin bitmaps/anim.png ../ASSETS/BITMAPS/ANIM.BIN
+../tools/png2bin bitmaps/player.png ../ASSETS/BITMAPS/PLAYER.BIN
