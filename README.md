658d0c3be35926511f6c4237428cc1ae36a3e108
jani-nykanen <jnyknn@gmail.com>
2019-06-06 22:45:01 +0300

    Code refactoring

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-One more time, with a feeling!
+## Plutonium Caverns
+
+**Plutonium Caverns** is a puzzle game for MS-DOS. For reasons.
+
+------
+
+## Builing
+
+OpenWatcom required. Put all the .c files to the project and build.
+
+------
+
+## Running
+
+Use DOSBOX
+
+-------
+
+## Code quality...
+
+
+...is bad, I know, but at least it works. I used global variables to improve the performace, even though it's considered a bad practice, I know.
+
+
+-------
+
+
+## License
+
+The code uses, say, **LGPL license**. The assets are for my own use only (except the font, feel free to use it).
+
 
 (c) 2019 Jani Nykänen
--- src/core/application.c
+++ src/core/application.c
@@ -31,7 +31,7 @@ static int16 frameSkip;
 static int16 stepCount;
 
 // Is running
-static bool running;
+static boolean running;
 
 
 // Dispose
@@ -49,6 +49,36 @@ static void dispose() {
     destroy_graphics();
     destroy_input();
 }
+
+// Update
+static void app_update(int16 steps) {
+    
+     // Update active scene
+    if(activeScene->update != NULL) {
+
+        activeScene->update(steps);
+    }
+
+    // Update transition
+    tr_update(steps);
+    // Update audio
+    audio_update(steps);
+}
+
+
+// Draw
+static void app_draw() {
+
+    if(activeScene->draw != NULL) {
+
+        // Draw active scene
+        activeScene->draw();
+    }
+
+    // Draw transition
+    tr_draw();
+}
+
 
 
 // Initialize
@@ -68,6 +98,63 @@ int16 init_application() {
     frameSkip = 1;
 
     return 0;
+}
+
+// Run
+void app_run() {
+
+    boolean updateFrame =false;
+    int16 i = 0;
+
+    // Initialize scenes
+    running = true;
+    for(i = 0; i < sceneCount; ++ i) {
+
+        if(scenes[i].init() == 1) {
+
+            running = false;
+            break;
+        }
+    }
+
+    // Start the main loop
+    stepCount = 0;
+    while(running) {
+
+        // Check frame skipping
+        updateFrame = false;
+        if(frameSkip == 0 || 
+           ++ stepCount > frameSkip) {
+
+            stepCount = 0;
+            updateFrame = true;
+        }
+
+        // Update & render the active scene
+        if(updateFrame 
+            && activeScene != NULL) {
+
+            // Update
+            app_update(frameSkip +1);
+            // Draw
+            app_draw();
+        }
+
+        // Wait for the vertical sync
+        vblank();
+
+        // Draw frame
+        draw_frame();
+    }
+
+    // Dispose application
+    dispose();
+
+    // Write error, if any
+    if(get_error() != NULL) {
+
+        printf("ERROR: %s\n", get_error());
+    }
 }
 
 
@@ -103,80 +190,9 @@ void app_change_scene(const char* name, void* param) {
 }
 
 
-// Run
-void app_run() {
-
-    bool updateFrame =false;
-    int16 i = 0;
-
-    // Initialize scenes
-    running = true;
-    for(i = 0; i < sceneCount; ++ i) {
-
-        if(scenes[i].init() == 1) {
-
-            running = false;
-            break;
-        }
-    }
-
-    // Start the main loop
-    stepCount = 0;
-    while(running) {
-
-        // Check frame skipping
-        updateFrame = false;
-        if(frameSkip == 0 || 
-           ++ stepCount > frameSkip) {
-
-            stepCount = 0;
-            updateFrame = true;
-        }
-
-        // Update & render the active scene
-        if(updateFrame 
-            && activeScene != NULL) {
-
-            // Update
-            if(activeScene->update != NULL) {
-
-                activeScene->update(frameSkip +1);
-            }
-            // Update transition
-            tr_update(frameSkip +1);
-            // Update audio
-            audio_update(frameSkip +1);
-
-            // Draw
-            if(activeScene->draw != NULL) {
-
-                activeScene->draw();
-
-                // Draw transition
-                tr_draw();
-            }
-        }
-
-        // Wait for the vertical sync
-        vblank();
-
-        // Draw frame
-        draw_frame();
-    }
-
-    // Dispose application
-    dispose();
-
-    // Write error, if any
-    if(get_error() != NULL) {
-
-        printf("ERROR: %s\n", get_error());
-    }
-}
-
-
 // Terminate
 void app_terminate() {
 
     running = false;
 }   
+
